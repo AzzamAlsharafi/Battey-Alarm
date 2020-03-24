4dd3c582001cacea3c5f07fceb0ff31475411dde
Azzam Alsharafi <azzamalsharafi@gmail.com>
2020-03-24 21:01:42 +0300

    Add: TextChangeListener on percentageEditText.
    This listener will check if the entered percentage is between 1 and 100.

--- app/src/main/java/com/azzam/batteryalarm/MainActivity.kt
+++ app/src/main/java/com/azzam/batteryalarm/MainActivity.kt
@@ -2,6 +2,10 @@ package com.azzam.batteryalarm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
 import com.azzam.batteryalarm.databinding.ActivityMainBinding
 
@@ -13,5 +17,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        addTextChangeListener()
+    }
+
+    private fun addTextChangeListener() {
+        binding.percentageEditText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(editable: Editable?) {
+                if(!editable.isNullOrEmpty()) {
+                    if (editable.toString().toInt() !in 1..100) {
+                        binding.percentageEditText.setText(100.toString())
+                    }
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+        })
     }
 }
